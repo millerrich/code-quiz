998e8252c0c5f586e7b6226999a569f5aea2a2fa
Miller Rich <william.miller.rich@gmail.com>
2020-08-23 21:13:49 -0500

    add more question objects

--- script.js
+++ script.js
@@ -9,3 +9,46 @@ const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
 const progress = document.getElementById("progress");
 const scoreContainer = document.getElementById("scoreContainer");
+
+let questions = [
+    {
+        question : "sample",
+        imgSrc : "#",
+        choiceA : "choice A",
+        choiceB : "choice B",
+        choiceC : "choice C",
+        correct : "C"
+    },
+    {
+        question : "sample 2",
+        imgSrc : "#",
+        choiceA : "choice A",
+        choiceB : "choice B",
+        choiceC : "choice C",
+        correct : "C"
+    },
+    {
+        question : "sample 3",
+        imgSrc : "#",
+        choiceA : "choice A",
+        choiceB : "choice B",
+        choiceC : "choice C",
+        correct : "C"
+    },
+    {
+        question : "sample 4",
+        imgSrc : "#",
+        choiceA : "choice A",
+        choiceB : "choice B",
+        choiceC : "choice C",
+        correct : "C"
+    },
+    {
+        question : "sample 5",
+        imgSrc : "#",
+        choiceA : "choice A",
+        choiceB : "choice B",
+        choiceC : "choice C",
+        correct : "C"
+    },
+]
