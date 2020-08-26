6330369af69aaacaf3749fee776d14895493e8f8
Miller Rich <william.miller.rich@gmail.com>
2020-08-25 20:51:55 -0500

    create function addQuestions, add event listener to question choice divs, start function checkAnswer

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const start = document.getElementById("start");
 // main quiz div set to display none initially
 const quiz = document.getElementById("quiz");
 // div for question to appear in
-const question = document.getElementById("question");
+var question = document.getElementById("question");
 // div for countdown timer
 const counter = document.getElementById("counter");
 // div for choices to answer question
@@ -16,43 +16,42 @@ const scoreContainer = document.getElementById("correctIncorrect");
 // object containing question arrays
 let questions = [
     {
-        question : "sample",
-        choiceA : "choice A",
-        choiceB : "choice B",
-        choiceC : "choice C",
-        choiceD : "choice D",
-        correct : "C"
+        question : "DOM stands for what?",
+        choiceA : "Document Object Model",
+        choiceB : "Dirty Odor Monster",
+        choiceC : "Direct Ordinance Master",
+        choiceD : "Dish Of Meatballs",
+        correct : choiceA
     },
     {
-        question : "sample 2",
-        choiceA : "choice A",
-        choiceB : "choice B",
-        choiceC : "choice C",
-        choiceD : "choice D",
-        correct : "C"
+        question : "In javascript var stands for what?",
+        choiceA : "Varsity",
+        choiceB : "Varnish",
+        choiceC : "Variable",
+        choiceD : "Variety",
+        correct : choiceC
     },
     {
-        question : "sample 3",
-        choiceA : "choice A",
-        choiceB : "choice B",
-        choiceC : "choice C",
-        choiceD : "choice D",
-        correct : "C"
+        question : "Which symbol is an alias for jQuery?",
+        choiceA : "*",
+        choiceB : "#",
+        choiceC : "&",
+        choiceD : "$",
+        correct : choiceD    },
+    {
+        question : "Where in the HTML document should you usually link your external Javascript file?",
+        choiceA : "In the head",
+        choiceB : "At the top of body section",
+        choiceC : "At the bottom of the body section",
+        choiceD : "Within the CSS link",
+        correct : choiceC
     },
     {
-        question : "sample 4",
-        choiceA : "choice A",
-        choiceB : "choice B",
-        choiceC : "choice C",
-        choiceD : "choice D",
-        correct : "C"
-    },
-    {
-        question : "sample 5",
-        choiceA : "choice A",
-        choiceB : "choice B",
-        choiceC : "choice C",
-        choiceD : "choice D",
+        question : "What does HTML stand for?",
+        choiceA : "Helpful Teacher Masking Laughter",
+        choiceB : "Hyper Text Markup Language",
+        choiceC : "Hello Today Make Lasagna",
+        choiceD : "Head To Mask List",
         correct : "C"
     },
 ]
@@ -64,6 +63,7 @@ start.addEventListener("click", function() {
     start.style.display = "none";
     quiz.style.display = "block";
     countdown();
+    addQuestions();
 
 });
 
@@ -84,3 +84,40 @@ function countdown() {
     }, 1000);
   }
 
+var i = 0;
+function addQuestions() {
+    // for (var i = 0; i < questions.length; i++) {}
+    question.innerText = questions[i].question
+    choiceA.innerText = questions[i].choiceA
+    choiceB.innerText = questions[i].choiceB
+    choiceC.innerText = questions[i].choiceC
+    choiceD.innerText = questions[i].choiceD
+    
+}
+
+
+
+function checkAnswer() {
+    alert("answer checked");
+    i++;
+    console.log(i);
+    addQuestions();
+    return
+    
+}
+choiceA.addEventListener("click", function() {
+    console.log("clicked A")
+    checkAnswer();
+});
+choiceB.addEventListener("click", function() {
+    console.log("clicked B")
+    checkAnswer();
+});
+choiceC.addEventListener("click", function() {
+    console.log("clicked C")
+    checkAnswer();
+});
+choiceD.addEventListener("click", function() {
+    console.log("clicked D")
+    checkAnswer();
+});
